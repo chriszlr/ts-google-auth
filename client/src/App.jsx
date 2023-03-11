1520627d21e0890bec2cad0022e2594eccaa2ead
Christopher Zöllner <chriszoelln@gmail.com>
2023-03-11 18:25:39 +0100

    setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <button>Sign in with Google</button>
+      <button>Get User</button>
+    </div>
+  );
+};
+
+export default App;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,38 @@
-import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import express, { Application } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import dotenv from "dotenv/config";
 import passport from "passport";
 import session from "express-session";
 import mongoose from "mongoose";
+
+const app: Application = express();
+const origin = "http://localhost:5173";
+
+// middleware
+app.use(
+  session({
+    secret: `${process.env.SESSION_SECRET}`,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin, credentials: true }));
+app.use(morgan("dev"));
+app.use(helmet());
+
+// connect to mongodb
+mongoose
+  .connect(`${process.env.MONGO_URI}`)
+  .then(() => console.log("MongoDB connected"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
