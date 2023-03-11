15ded8770601e5ea20aca54af1d4f5d9d328b8cc
Christopher Zöllner <chriszoelln@gmail.com>
2023-03-11 13:21:54 +0100

    package.json

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv/config";
+import passport from "passport";
+import session from "express-session";
+import mongoose from "mongoose";
